a237b4c9e69f740e0de36f69cf32590c2c758da8
Rowan Dash <pokemonmaster9967@gmail.com>
2021-10-05 14:32:15 +0100

    Update UIGreetings.java
    Comments

--- Worksheet 2/UIGreetings.java
+++ Worksheet 2/UIGreetings.java
@@ -1,8 +1,13 @@
 import javax.swing.JOptionPane;
 
+/* A program that creates a window and greets the user*/
 public class UIGreetings {
     public static void main(String[] args) {
+
+        //display an input prompt
         String name = JOptionPane.showInputDialog("What is your name?");
+
+        //change to an output prompt
         JOptionPane.showMessageDialog(null, "Hello " + name, "Greetings fellow human", 0);
     }
 }
