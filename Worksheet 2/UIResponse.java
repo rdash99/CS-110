5ed6f278a42a1701f2a17ec291ae03026abf61c5
Rowan Dash <pokemonmaster9967@gmail.com>
2021-10-05 14:25:36 +0100

    Create UIResponse.java

---
+++ Worksheet 2/UIResponse.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class UIResponse {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Your name here",
+        "I'm saying Hello...", 0);
+    }
+}
