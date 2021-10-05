6b3714a5a5ecbd857842fc6f62f17097e530303d
Rowan Dash <pokemonmaster9967@gmail.com>
2021-10-05 13:51:16 +0100

    Update MilesToKilometers.java
    Add ability to format the number of decimal points

--- Worksheet 1/MilesToKilometers.java
+++ Worksheet 1/MilesToKilometers.java
@@ -1,13 +1,19 @@
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class MilesToKilometers {
     public static void main(String[] args) {
-        //constants
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+
         final double DISTANCE_BETWEEN_CAMPUS = 5.5; // Distance in miles
         final double CONVERSION_CONSTANT = 1.609;
 
         //Convert from miles to kmph
-        var kilometers = DISTANCE_BETWEEN_CAMPUS * CONVERSION_CONSTANT;
+        double kilometers = DISTANCE_BETWEEN_CAMPUS * CONVERSION_CONSTANT;
 
         //print result
-        System.out.println("The distance between campuses is " + kilometers + "km");
+        System.out.println("The distance between campuses is " + df.format(kilometers) + "km");
     }
 }
