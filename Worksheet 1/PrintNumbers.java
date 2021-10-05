854dc078a7b7954ede34306611d6f232ef215f47
Rowan Dash <pokemonmaster9967@gmail.com>
2021-10-05 13:27:51 +0100

    Create PrintNumbers.java

---
+++ Worksheet 1/PrintNumbers.java
@@ -0,0 +1,9 @@
+public class PrintNumbers {
+    public static void main(String[] args) {
+        System.out.println(7.5);
+        System.out.println(4 + 3.5);
+        System.out.println("4 + 3.5");
+        System.out.println(17 * 1.75);
+        System.out.println("Our solar system has " + 8 + " planets");
+    }
+}
