521718dad9a2662d7a78ec58b9d173108fae852a
Rowan Dash <pokemonmaster9967@gmail.com>
2021-10-06 15:55:30 +0100

    Create MilimetersToInches.java

---
+++ Tutorial/MilimetersToInches.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MilimetersToInches {
+    public static void main(String[] args) {
+        final double MM_PER_INCH = 25.4;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the number of milimeters with a decimal point:");
+        double toConvert = in.nextDouble();
+
+        double coverted = toConvert/MM_PER_INCH;
+        
+        System.out.println(coverted + " inches!");
+    }
+}
