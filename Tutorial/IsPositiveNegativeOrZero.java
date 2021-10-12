3ac8ce42496e3acd593b316600e7bf5b9e8a33e7
Rowan Dash <pokemonmaster9967@gmail.com>
2021-10-12 21:30:10 +0100

    Create IsPositiveNegativeOrZero.java

---
+++ Tutorial/IsPositiveNegativeOrZero.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IsPositiveNegativeOrZero {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a whole number:");
+        int number = in.nextInt();
+
+        if (number == 0){
+            System.out.println("The number is zero.");
+        }
+        if (number < 0){
+            System.out.println("Number is negative.");
+        }
+        if (number > 0){
+            System.out.println("The number is positive.");
+        }
+    }
+}
