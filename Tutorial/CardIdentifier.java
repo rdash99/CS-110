9eee9d436505a0c3ef8bfa3eb6e3d75fcdf0e24c
Rowan Dash <pokemonmaster9967@gmail.com>
2021-10-13 20:39:54 +0100

    Update CardIdentifier.java
    Program now outputs card - added checks for card identification and updated comments

--- Tutorial/CardIdentifier.java
+++ Tutorial/CardIdentifier.java
@@ -6,7 +6,10 @@ import java.util.Scanner;
 public class CardIdentifier {
     public static void main(String[] args){
 
-        List EXPECTED_FIRST_CHAR = new ArrayList();
+        String firstString = "";
+        String secondWord = "";
+
+        final List EXPECTED_FIRST_CHAR = new ArrayList();
 
         int i = 0;
         while (i<=9){
@@ -21,7 +24,7 @@ public class CardIdentifier {
 
         
 
-        List EXPECTED_LAST_CHAR = new ArrayList();
+        final List EXPECTED_LAST_CHAR = new ArrayList();
 
         EXPECTED_LAST_CHAR.add("S");
         EXPECTED_LAST_CHAR.add("C");
@@ -69,8 +72,50 @@ public class CardIdentifier {
                 char2 = code.substring(1,2);
             }
         }
+        //Check what the first character is
         if (char1.equals("0")){
-            System.out.println("Yay"); //TODO Work out meaning of card code
+            firstString = "the 10 of ";
+        }else if(char1.equals("1")){
+            firstString = "the 1 of ";
+        }else if(char1.equals("2")){
+            firstString = "the 2 of ";
+        }else if(char1.equals("3")){
+            firstString = "the 3 of ";
+        }else if(char1.equals("4")){
+            firstString = "the 4 of ";
+        }else if (char1.equals("5")){
+            firstString = "the 5 of ";
+        }else if (char1.equals("6")){
+            firstString = "the 6 of";
+        }else if (char1.equals("7")){
+            firstString = "the 7 of ";
+        }else if (char1.equals("8")){
+            firstString = "the 8 of ";
+        }else if(char1.equals("9")){
+            firstString = "the 9 of ";
+        }else if (char1.equals("J")){
+            firstString = "the Jack of ";
+        }else if (char1.equals("Q")){
+            firstString = "the Queen of ";
+        }else if (char1.equals("K")){
+            firstString = "the King of ";
+        }else{
+            firstString = "the Ace of ";
         }
+        //System.out.println(firstString);
+
+        //Check what the second character is
+        if (char2.equals("H")){
+            secondWord = "Hearts.";
+        }else if (char2.equals("C")){
+            secondWord = "Clubs.";
+        }else if (char2.equals("D")){
+            secondWord = "Diamonds.";
+        }else if (char2.equals("S")){
+            secondWord = "Spades.";
+        }
+        //System.out.println(secondWord);
+
+        System.out.println("Your card is " + firstString + secondWord);
     }
 }
