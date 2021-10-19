ca22cdc5a1d63d14e427d9edcdf34c69467a55cf
Rowan Dash <pokemonmaster9967@gmail.com>
2021-10-19 13:02:47 +0100

    Create ManhattenAddress.java

---
+++ Lecture Programs/ManhattenAddress.java
@@ -0,0 +1,13 @@
+public class ManhattenAddress {
+    public static void main(String[] args) {
+        String streetName = "5th Avenue";
+        int buildingNum = 1314;
+        int divisor = 20;
+
+        // Work out divisor value
+        if (streetName.equals("5th Avenue") || streetName.equals("5th Avenue")
+                || streetName.equals("Riverside Drive")) {
+            divisor = 10;
+        }
+    }
+}
