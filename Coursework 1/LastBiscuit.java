7c4de846abcf16d4fc6f4fd821d168cdbf611843
Rowan Dash <pokemonmaster9967@gmail.com>
2021-10-27 20:10:33 +0100

    Update LastBiscuit.java
    Update to level 3

--- Coursework 1/LastBiscuit.java
+++ Coursework 1/LastBiscuit.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class LastBiscuit {
 
-  /* A program to create and run the game of last biscuit.
-  Written by Rowan Dash (2106866). SpLD marking regulations apply */
-  public static void main(String[] args) {
+  /*
+   * A program to create and run the game of last biscuit. Written by Rowan Dash
+   * (2106866). SpLD marking regulations apply
+   */
+  public static void main(final String[] args) {
     final int BARREL_1_INITIAL = 6;
     final int BARREL_2_INITIAL = 8;
     int barrel1 = BARREL_1_INITIAL;
@@ -12,40 +14,87 @@ public class LastBiscuit {
     int turn = 1;
     String barrel = "";
     int numberTaken = 0;
+    boolean legal = false;
     Scanner in = new Scanner(System.in);
 
     // Main game loop
-    while (barrel1 >= 0 && barrel2 >= 0) {
+    while (barrel1
+        >= 0
+        && barrel2
+            >= 0) {
       // Print statements
-      System.out.println("Biscuits Left - Barrel 1: " + barrel1);
-      System.out.println("Biscuits Left - Barrel 2: " + barrel2);
-      System.out.println("Player Turn: " + turn);
+      System.out.println("Biscuits Left - Barrel 1: "
+          + barrel1);
+      System.out.println("Biscuits Left - Barrel 2: "
+          + barrel2);
+      System.out.println("Player Turn: "
+          + turn);
 
-      // Choose a barrel
-      System.out.print(
-        "Choose a barrel: barrel1 (one), barrel2 (two), or both (both), or skip turn (skip)? "
-      );
-      barrel = in.next();
-      barrel.toLowerCase();
-
-      // Barrel input checks
-      while (
-        !barrel.equals("one") && !barrel.equals("two") && !barrel.equals("both")
-      ) {
-        System.out.print(
-          "Choose a barrel: barrel1 (one), barrel2 (two), or both (both), or skip turn (skip)? "
-        );
+      // Number legality checking loop
+      while (!legal) {
+        // Choose a barrel
+        System.out.print("Choose a barrel: barrel1 (one), barrel2 (two), "
+            + "or both (both),"
+            + " or skip turn (skip)? ");
         barrel = in.next();
         barrel.toLowerCase();
-      }
 
-      // Get the number of biscuits removed
-      System.out.print("How many biscuits are you taking? ");
-      while (!in.hasNextInt()) {
-        System.out.print("Please input an integer: ");
-        in.next();
+        // Barrel input checks
+        while (!barrel.equals("one")
+            && !barrel.equals("two")
+            && !barrel.equals("both")) {
+          System.out.print("Choose a barrel: barrel1 (one), barrel2 (two), "
+              + "or both (both), "
+              + "or skip turn (skip)? ");
+          barrel = in.next();
+          barrel.toLowerCase();
+        }
+
+        // Get the number of biscuits to be removed
+        //Check whether the input number is legal
+        System.out.print("How many biscuits are you taking? ");
+        while (!in.hasNextInt()) {
+          System.out.print("Please input an integer: ");
+          in.next();
+        }
+        numberTaken = in.nextInt();
+
+        // Check if the number input is negative
+        if (numberTaken
+            <= 0) {
+          System.out.print("Sorry, that's not a legal number of "
+              + "biscuits for that/those barrel(s)\n");
+          barrel = "";
+        }
+        // Check if there are enough biscuits in the barrel/s
+        if (barrel.equals("both")) {
+          if (numberTaken
+              > barrel1
+              && numberTaken
+                  > barrel2) {
+            System.out.print("Sorry, that's not a legal number of "
+                + "biscuits for that/those barrel(s)\n");
+          } else {
+            legal = true;
+          }
+        } else if (barrel.equals("one")) {
+          if (numberTaken
+              > barrel1) {
+            System.out.print("Sorry, that's not a legal number of "
+                + "biscuits for that/those barrel(s)\n");
+          } else {
+            legal = true;
+          }
+        } else if (barrel.equals("two")) {
+          if (numberTaken
+              > barrel2) {
+            System.out.print("Sorry, that's not a legal number"
+                + " of biscuits for that/those barrel(s)\n");
+          } else {
+            legal = true;
+          }
+        }
       }
-      numberTaken = in.nextInt();
 
       // Remove the correct number of biscuits from the barrels
       if (barrel.equals("both")) {
@@ -57,38 +106,32 @@ public class LastBiscuit {
         barrel2 -= numberTaken;
       }
 
-      // Check barrel contents and update the number to zero if negative
-      if (barrel1 < 0) {
-        barrel1 = 0;
-      } else if (barrel2 < 0) {
-        barrel2 = 0;
-      } else if (barrel1 < 0 && barrel2 < 0) {
-        barrel1 = 0;
-        barrel2 = 0;
-        System.out.println("Biscuits Left - Barrel 1: " + barrel1);
-        System.out.println("Biscuits Left - Barrel 2: " + barrel2);
-        System.out.println("Winner is player " + turn);
-        break;
-      }
-
       // Win check
-      if (barrel1 == 0 && barrel2 == 0) {
-        System.out.println("Biscuits Left - Barrel 1: " + barrel1);
-        System.out.println("Biscuits Left - Barrel 2: " + barrel2);
-        System.out.println("Winner is player " + turn);
+      if (barrel1
+          == 0
+          && barrel2
+              == 0) {
+        System.out.println("Biscuits Left - Barrel 1: "
+            + barrel1);
+        System.out.println("Biscuits Left - Barrel 2: "
+            + barrel2);
+        System.out.println("Winner is player "
+            + turn);
         break;
       }
 
       // Swap active player
-      if (turn == 1) {
+      if (turn
+          == 1) {
         turn = 2;
       } else {
         turn = 1;
       }
-      // Reset the taken number of biscuits
+      // Reset the taken number of biscuits and the chosen barrel
+      //re-enable the legality check loop
       numberTaken = 0;
-
       barrel = "";
+      legal = false;
     }
   }
 }
