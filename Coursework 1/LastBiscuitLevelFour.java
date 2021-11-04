3ec966affbcceff9390161b259a1e7d6a56ed462
Rowan Dash <pokemonmaster9967@gmail.com>
2021-11-04 12:16:45 +0000

    Update LastBiscuitLevelFour.java

--- Coursework 1/LastBiscuitLevelFour.java
+++ Coursework 1/LastBiscuitLevelFour.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class LastBiscuitLevelFour {
     /*
-     * A program to create and run the game of last biscuit. Written by Rowan Dash
-     * (2106866). SpLD marking regulations apply
+     * A program to create and run the game of last biscuit.
+     * Written by Rowan Dash (2106866).
+     * SpLD marking regulations apply
      */
     public static void main(final String[] args) {
         final int BARREL_1_INITIAL = 6;
@@ -9,9 +12,13 @@ public class LastBiscuitLevelFour {
         int barrel1 = BARREL_1_INITIAL;
         int barrel2 = BARREL_2_INITIAL;
         int turn = 1;
-        String barrel = "";
+        String barrel = "none";
         int numberTaken = 0;
         boolean legal = false;
+        int userOneSkipped = 0;
+        int userTwoSkipped = 0;
+        boolean skip = false;
+        boolean barrelLoop = true;
         Scanner in = new Scanner(System.in);
 
         // Main game loop
@@ -29,71 +36,137 @@ public class LastBiscuitLevelFour {
 
             // Number legality checking loop
             while (!legal) {
-                // Choose a barrel
-                System.out.print("Choose a barrel: barrel1 (one), barrel2 (two), "
-                        + "or both (both),"
-                        + " or skip turn (skip)? ");
-                barrel = in.next();
-                barrel.toLowerCase();
+                while (barrelLoop) {
+                    // Choose a barrel
+                    /*
+                     * System.out.print("Choose a barrel: barrel1 (one),"
+                     * + " barrel2 (two), "
+                     * + "or both (both),"
+                     * + " or skip turn (skip)? ");
+                     * barrel = in.nextLine().trim();
+                     * barrel.toLowerCase();
+                     */
 
-                // Barrel input checks
-                while (!barrel.equals("one")
-                        && !barrel.equals("two")
-                        && !barrel.equals("both")
-                        && !barrel.equals("skip")) {
-                    System.out.print("Choose a barrel: barrel1 (one), barrel2 (two), "
-                            + "or both (both), "
-                            + "or skip turn (skip)? ");
-                    barrel = in.next();
-                    barrel.toLowerCase();
+                    // Barrel input checks
+                    while (!barrel.equals("one")
+                            && !barrel.equals("two")
+                            && !barrel.equals("both")
+                            && !barrel.equals("skip")) {
+                        System.out.print("Choose a barrel: barrel1 (one),"
+                                + " barrel2 (two), "
+                                + "or both (both), "
+                                + "or skip turn (skip)? ");
+
+                        barrel = in.nextLine();
+                        barrel.toLowerCase();
+
+                    }
+
+                    if (turn
+                            == 1) {
+                        if (barrel.equals("skip")) {
+                            if (userOneSkipped
+                                    == 1) {
+                                System.out.println("Sorry you've used your skip.");
+                                barrel = "none";
+                                skip = false;
+                            } else if (userOneSkipped
+                                    == 0) {
+                                barrelLoop = false;
+                                skip = true;
+                                userOneSkipped = 1;
+                            }
+                        } else {
+                            barrelLoop = false;
+                            skip = false;
+                        }
+                    }
+
+                    if (turn
+                            == 2) {
+                        if (barrel.equals("skip")) {
+                            if (userTwoSkipped
+                                    == 1) {
+                                System.out.println("Sorry you've used your skip.");
+                                barrel = "none";
+                                skip = false;
+                            } else if (userTwoSkipped
+                                    == 0
+                                    && barrel.equals("skip")) {
+                                barrelLoop = false;
+                                skip = true;
+                                userTwoSkipped = 1;
+                            }
+                        } else {
+                            barrelLoop = false;
+                            skip = false;
+                        }
+                    }
                 }
 
-                // Get the number of biscuits to be removed
-                //Check whether the input number is legal
-                System.out.print("How many biscuits are you taking? ");
-                while (!in.hasNextInt()) {
-                    System.out.print("Please input an integer: ");
-                    in.next();
-                }
-                numberTaken = in.nextInt();
+                if (!skip) {
+                    // Get the number of biscuits to be removed
+                    //Check whether the input number is legal
+                    System.out.print("How many biscuits are you taking? ");
+                    while (!in.hasNextInt()) {
+                        System.out.print("Please input an integer: ");
+                        in.next();
+                    }
+                    numberTaken = in.nextInt();
+                    in.nextLine();
 
-                // Check if the number input is negative
-                if (numberTaken
-                        <= 0) {
-                    System.out.print("Sorry, that's not a legal number of "
-                            + "biscuits for that/those barrel(s)\n");
-                    barrel = "";
-                }
-                // Check if there are enough biscuits in the barrel/s
-                if (barrel.equals("both")) {
+                    // Check if the number input is negative
                     if (numberTaken
-                            > barrel1
-                            && numberTaken
-                                    > barrel2) {
-                        System.out.print("Sorry, that's not a legal number of "
-                                + "biscuits for that/those barrel(s)\n");
-                    } else {
-                        legal = true;
+                            <= 0) {
+                        System.out.println("Sorry, that's not a legal number of "
+                                + "biscuits for that/those barrel(s)");
+                        barrel = "none";
+                        legal = false;
+                        barrelLoop = true;
                     }
-                } else if (barrel.equals("one")) {
-                    if (numberTaken
-                            > barrel1) {
-                        System.out.print("Sorry, that's not a legal number of "
-                                + "biscuits for that/those barrel(s)\n");
-                    } else {
-                        legal = true;
+                    // Check if there are enough biscuits in the barrel/s
+                    if (barrel.equals("both")) {
+                        if (numberTaken
+                                > barrel1
+                                && numberTaken
+                                        > barrel2) {
+                            System.out.println("Sorry, that's not a legal"
+                                    + " number of "
+                                    + "biscuits for that/those barrel(s)");
+                            legal = false;
+                            barrelLoop = true;
+                            barrel = "none";
+                        } else {
+                            legal = true;
+                        }
+                    } else if (barrel.equals("one")) {
+                        if (numberTaken
+                                > barrel1) {
+                            System.out.println("Sorry, that's not a legal"
+                                    + " number of "
+                                    + "biscuits for that/those barrel(s)");
+                            legal = false;
+                            barrelLoop = true;
+                            barrel = "none";
+                        } else {
+                            legal = true;
+                        }
+                    } else if (barrel.equals("two")) {
+                        if (numberTaken
+                                > barrel2) {
+                            System.out.println("Sorry, that's not a legal number"
+                                    + " of biscuits for that/those barrel(s)");
+                            legal = false;
+                            barrelLoop = true;
+                            barrel = "none";
+                        } else {
+                            legal = true;
+                        }
                     }
-                } else if (barrel.equals("two")) {
-                    if (numberTaken
-                            > barrel2) {
-                        System.out.print("Sorry, that's not a legal number"
-                                + " of biscuits for that/those barrel(s)\n");
-                    } else {
-                        legal = true;
-                    }
+                } else {
+                    legal = true;
                 }
             }
-
             // Remove the correct number of biscuits from the barrels
             if (barrel.equals("both")) {
                 barrel1 -= numberTaken;
@@ -103,7 +176,6 @@ public class LastBiscuitLevelFour {
             } else {
                 barrel2 -= numberTaken;
             }
-
             // Win check
             if (barrel1
                     == 0
@@ -128,8 +200,10 @@ public class LastBiscuitLevelFour {
             // Reset the taken number of biscuits and the chosen barrel
             // re-enable the legality check loop
             numberTaken = 0;
-            barrel = "";
+            barrel = "none";
             legal = false;
+            skip = false;
+            barrelLoop = true;
         }
     }
 }
