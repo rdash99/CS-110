65dc88326699ceb25898a082e0a6fb05fdc49a27
Rowan Dash <pokemonmaster9967@gmail.com>
2021-11-16 14:57:19 +0000

    Create RegularPolygonTests.java

---
+++ Worksheet 8/RegularPolygonTests.java
@@ -0,0 +1,7 @@
+public class RegularPolygonTests {
+    public static void main(String[] args) {
+        RegularPolygon rp = new RegularPolygon(5, 4);
+        System.out.println(rp.getArea());
+        System.out.println(rp.getNumberOfSides());
+    }
+}
