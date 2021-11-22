b839d49e71f985071aa9a907e4ffbd2be07f863d
Rowan Dash <pokemonmaster9967@gmail.com>
2021-11-22 14:05:30 +0000

    Update City.java

--- Coursework 2/City.java
+++ Coursework 2/City.java
@@ -1,3 +1,30 @@
 public class City {
+    private String name;
+    private int population;
+    private int timeZone;
+    private String megacityOutput;
 
+    public City(String name, int population, int timeZone) {
+        this.name = name;
+        this.population = population;
+        this.timeZone = timeZone;
+        if (population
+                >= 10000000) {
+            this.megacityOutput = "";
+        } else {
+            this.megacityOutput = "NOT";
+        }
+    }
+
+    public String toString() {
+        return "City: "
+                + this.name
+                + " has population: "
+                + this.population
+                + " and is in time zone: "
+                + this.timeZone
+                + ". It IS "
+                + megacityOutput
+                + " a megacity";
+    }
 }
