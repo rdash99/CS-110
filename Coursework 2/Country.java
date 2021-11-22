dfa51aa5ee0a12bf0ef56c5d9950cdd556ecfc47
Rowan Dash <pokemonmaster9967@gmail.com>
2021-11-22 14:05:33 +0000

    Update Country.java

--- Coursework 2/Country.java
+++ Coursework 2/Country.java
@@ -1,3 +1,34 @@
-public class Country {
+import java.util.ArrayList;
 
+public class Country {
+    private String name;
+    private int population;
+    private int popsOutOfCities;
+    private ArrayList<> cities = new ArrayList<City>();
+
+    public Country(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    public void addCity(String city, int population, int timeZone) {
+        cities.add(new City(city, population, timeZone));
+        this.popsOutOfCities += population;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        System.out.println(this.name
+                + " total population: "
+                + this.population
+                + ", population outside listed cities: "
+                + this.popsOutOfCities
+                + ", with cities");
+        for (City city : cities) {
+            System.out.println(city.toString());
+        }
+    }
 }
