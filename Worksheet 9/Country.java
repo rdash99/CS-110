e97fbcb7c4187ee9f8bee25d1737428ff2ec6094
Rowan Dash <pokemonmaster9967@gmail.com>
2021-11-23 13:32:21 +0000

    Create Country.java

---
+++ Worksheet 9/Country.java
@@ -0,0 +1,30 @@
+public class Country {
+    private String name;
+    private HighPoint highPoints;
+
+    public Country(String name, HighPoint highPoints) {
+        this.name = name;
+        this.highPoints = highPoints;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHighestPoint() {
+        return this.highPoints.getHeight();
+    }
+
+    public String getHighestPointName() {
+        return this.highPoints.getName();
+    }
+
+    public String toString() {
+        return this.name
+                + " has a high point at "
+                + this.highPoints.getName()
+                + " at "
+                + this.highPoints.getHeight()
+                + " meters.";
+    }
+}
