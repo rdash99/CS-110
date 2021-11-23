4f4d0642fbd0511ad78e882b0423922a0141b3c4
Rowan Dash <pokemonmaster9967@gmail.com>
2021-11-23 13:32:18 +0000

    Create Continent.java

---
+++ Worksheet 9/Continent.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Continent {
+    private ArrayList<Country> countries = new ArrayList<Country>();
+
+    public void addCountry(Country country) {
+        countries.add(country);
+    }
+
+    public void getHighestPoint() {
+        int highest = countries.get(0).getHighestPoint();
+        String countryName = "";
+        String pointName = "";
+        Country highestCountry = countries.get(0);
+        for (Country country : countries) {
+            if (country.getHighestPoint()
+                    > highest) {
+                highest = country.getHighestPoint();
+                countryName = country.getName();
+                pointName = country.getHighestPointName();
+                highestCountry = country;
+
+            }
+        }
+        System.out.println(highestCountry.getName()
+                + " has the highest point at "
+                + highestCountry.getHighestPointName()
+                + ". Which is "
+                + highest
+                + " meters.");
+    }
+
+    public void printCountries() {
+        for (Country country : countries) {
+            System.out.println(country);
+        }
+    }
+}
