c38310ae8bc6848c105187f5442770a1951bd47a
Rowan Dash <pokemonmaster9967@gmail.com>
2021-11-23 18:41:40 +0000

    Level 3 hidden test attempted fix - failure
    Comments and additional validation to attempt to fix level 3 hidden test

--- Coursework 2/City.java
+++ Coursework 2/City.java
@@ -6,6 +6,7 @@ public class City {
     private int timeZone;
     private String megacityOutput;
 
+    //constructor
     public City(String name, int population, int timeZone) {
         this.name = name;
         this.population = population;
@@ -18,10 +19,17 @@ public class City {
         }
     }
 
+    //return the population of the city
+    public int getPopulation() {
+        return this.population;
+    }
+
+    //return the name of the city
     public String getName() {
         return this.name;
     }
 
+    //calculate the time difference between this city and another
     public int timeDifference(City other) {
         if (this.timeZone
                 == other.timeZone) {
@@ -36,6 +44,7 @@ public class City {
         }
     }
 
+    //return a formatted string
     public String toString() {
         return this.name
                 + ": has population "
@@ -45,6 +54,21 @@ public class City {
                 + ". It IS"
                 + megacityOutput
                 + " a megacity";
+    }
+
+    //check if the data in this city is valid
+    public boolean isLegalData() {
+        if (this.population
+                <= 0
+                || this.timeZone
+                        <= -12
+                || this.timeZone
+                        >= 11
+                || this.name.equals(null)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
 }
--- Coursework 2/Country.java
+++ Coursework 2/Country.java
@@ -11,21 +11,46 @@ public class Country {
     private int popsInCities;
     private int cityIndex;
 
+    // Constructor
     public Country(String name, int population) {
         this.name = name;
-        this.population = population;
+        if (population
+                < 0) {
+            this.population = 0;
+        } else {
+            this.population = population;
+        }
+        //intialise the output string variable
         this.outString = "";
     }
 
+    //add a city object to the country
     public void addCity(String city, int population, int timeZone) {
+        if (population
+                <= 0) {
+            population = 0;
+        }
+        if (timeZone
+                < -12
+                || timeZone
+                        > 11) {
+            timeZone = 0;
+        }
+        if (city.equals("")
+                || city.equals(" ")
+                || city.equals(null)) {
+            city = null;
+        }
         cities.add(new City(city, population, timeZone));
         this.popsInCities += population;
     }
 
+    //Return the Country's name
     public String getName() {
         return name;
     }
 
+    //String formatting for the output
     public String toString() {
         this.popsOutOfCities = this.population
                 - this.popsInCities;
@@ -44,6 +69,7 @@ public class Country {
         return this.outString;
     }
 
+    //retrive a city by name
     public City getCityByName(String cityName) {
         for (City city : cities) {
             if (city.getName().equals(cityName)) {
@@ -52,5 +78,40 @@ public class Country {
             }
         }
         return null;
+    }
+
+    //delete a city by name
+    public boolean deleteCity(String cityName) {
+        for (City city : cities) {
+            if (city.getName().equals(cityName)) {
+                this.cityIndex = cities.indexOf(city);
+                this.popsInCities -= city.getPopulation();
+                cities.remove(city);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Check if the data in the Country object is legal
+    public boolean isLegalData() {
+        if (this.population
+                <= 0) {
+            return false;
+        }
+        if (this.popsInCities
+                > this.population) {
+            return false;
+        }
+        if (this.popsInCities
+                <= 0) {
+            return false;
+        }
+        if (this.name.equals("")
+                || this.name.equals(" ")
+                || this.name.equals(null)) {
+            return false;
+        }
+        return true;
     }
 }
